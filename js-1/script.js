3fdeb4b44a9a0616233e19e2715c568243582a5a
Lili Legran <lili_akhunyanova@icloud.com>
2018-08-25 16:35:50 +0300

    deleted some folders

--- js-1/script.js
+++
@@ -1,9 +0,0 @@
-menu.onclick = function myFunction() {
-    var x = document.getElementById('myRunnav');
-
-    if (x.className === "runNav") {
-        x.className += " responsive";
-    } else {
-        x.className = "runNav";
-    }
-}
--- js1/script.js
+++ js1/script.js
@@ -1,8 +1,6 @@
 menu.onclick = function myFunction() {
     var x = document.querySelector(".menu");
 
-    console.log("21");
-
     if (x.className === "menu") {
     x.className += " open";
     } else {
